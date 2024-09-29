ae5f1ba5c05620b9eeda862da24dde15d2ff99e8
michalBirenzwaig <michalostrov488@gmail.com>
2024-09-30 00:12:33 +0300

    lesson4

---
+++ lesson4/class_task.py
@@ -0,0 +1,10 @@
+# # task_2
+# my_string1 = input("Your string 1: ")
+# print(my_string1[-1] + my_string1[:-1])
+# # task_3
+# my_string2 = input("Your string 2: ")
+# print(my_string2[1::2])
+# task_5
+word = input("Your word is: ")
+print(len(word)>=6)
+print(word[:3].upper()+word[3:-3].lower()+word[-3:].upper())
